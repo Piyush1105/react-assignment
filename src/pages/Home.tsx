f4d60ab8e1295760db5497a033b724cfc26c91f5
Piyush Yadav <piyushyadav1105@gmail.com>
2025-06-05 08:54:07 +0530

    some css fixes

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -19,7 +19,7 @@ function Home() {
 
   return (
     <div className="relative">
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6 p-6">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-5 gap-2 p-6">
         {products.map((product) => (
           <ProductCard key={product?.id} product={product} onClick={() => handleCardClick(product?.id)} />
         ))}
